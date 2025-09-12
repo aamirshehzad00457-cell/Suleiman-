1f5b2bc1392da541ecf1541a017e859ff820cdc6
aamirshehzad00457-cell <aamirshehzad00457@gmail.com>
2025-09-12 07:50:05 +0500

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = mysqli_connect('shareddb-x.hosting.stackcp.net','raj','12345678@','tiwari-313537e38d');
-
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
-?>
